1353ec50344787d71861cbb76c125e31ccc9506a
sahil6585 <136334175+sahil6585@users.noreply.github.com>
2023-11-05 19:38:51 +0530

    Add files via upload

---
+++ Print the ASCII Value of the Character/Find ascii value of given number.kt
@@ -0,0 +1,12 @@
+//Find ascii value of given number
+
+package second
+
+fun main()
+{
+
+    var c='9'
+    var ascii= c.toInt()
+
+    println("$c = ascii is $ascii")
+}
---
+++ Print the ASCII Value of the Character/Find character value from ascii.kt
@@ -0,0 +1,15 @@
+//Find character value from ascii
+
+package second
+
+
+fun main()
+{
+
+    var ascii=65
+
+    var char = ascii.toChar()
+
+    println("$ascii to Char = $char")
+
+}
